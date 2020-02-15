85666d83dc584bff0239c27adeeacfce041c3554
Anton Brisilin <brisilant@yandex.ru>
2020-02-15 16:58:00 +0300

    added solution for task 5

--- Task1/plotting.m
+++ Task1/plotting.m
@@ -2,4 +2,5 @@ plot(sim1.time,sim1.data,sim2.time,sim2.data,'LineWidth',2);
 hold on;
 fplot(sol,'LineWidth',2);
 axis([0 10 0 inf]);
-legend('Simulink model 1','Simulink model 2','Exact solution')
+legend('Simulink model 1','Simulink model 2',
+        'Exact solution')
